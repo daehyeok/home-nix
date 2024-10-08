13da230a7cd08fcb13d1d9ffd36d3ce8d50614c3
daehyeok mun <daehyeok@daehyeokui-MacBookAir.local>
2024-10-07 22:43:22 -0700

    add all-the-icons font

--- home.nix
+++ home.nix
@@ -4,6 +4,13 @@
   lib,
   ...
 }:
+let
+  vterm-build-deps = with pkgs; [
+    cmake
+    glibtool
+  ];
+  home = builtins.getEnv "HOME";
+in
 with lib;
 {
   imports = [
@@ -12,51 +19,49 @@ with lib;
   ];
 
   config = {
-
-    nixpkgs.overlays = [
-      (import (
-        builtins.fetchTarball {
-          url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-        }
-      ))
-    ];
-
     # Home Manager needs a bit of information about you and the
     # paths it should manage.
     home = {
       username = "daehyeok";
       homeDirectory = "/Users/daehyeok";
-      stateVersion = "22.05";
-      packages = with pkgs; [
-        kitty
-        (nerdfonts.override { fonts = [ "Hack" ]; })
-      ];
+      stateVersion = "23.05";
+      packages =
+        with pkgs;
+        [
+          fontconfig
+          (nerdfonts.override { fonts = [ "Hack" ]; })
+          emacs-all-the-icons-fonts
+          gettext
+          cargo-edit
+          pkg-config
+          openssl
+        ]
+        ++ vterm-build-deps;
     };
 
     programs = {
       # Let Home Manager install and manage itself.
       home-manager.enable = true;
-      starship.enable = true;
+      zsh = {
+        enable = true;
+        initExtraFirst = ''
+          source /etc/static/bashrc  2> /dev/null
+          source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+          source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
+          eval "$(/opt/homebrew/bin/brew shellenv)"
+        '';
+      };
       git.userEmail = "daehyeok@gmail.com";
       direnv = {
         enable = true;
         nix-direnv.enable = true;
-      };
-      zinit.enable = true;
-      zsh.initExtra = "typeset -g LC_ALL=en_US.UTF-8";
-      emacs = {
-        enable = true;
-        package = pkgs.emacsNativeComp;
-        extraPackages = (epkgs: [ epkgs.vterm ]);
       };
     };
 
     xdg.enable = true;
 
     modules.dev = {
-      dart = {
-        enable = true;
-      };
       nix = {
         enable = true;
       };

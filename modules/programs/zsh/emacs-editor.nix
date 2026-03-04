09894498455bdabd1fcf054448cf9c9d771742c4
Daehyeok Mun <daehyeok@google.com>
2026-03-03 16:22:18 -0800

    feat(nix): Add emacs-editor script to home.packages when enabled

--- modules/programs/zsh/emacs-editor.nix
+++ modules/programs/zsh/emacs-editor.nix
@@ -1,5 +1,25 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 with lib;
+let
+  cfg = config.programs.zsh.emacs-editor;
+  emacs-editor-script = pkgs.writeScriptBin "emacs-editor" ''
+    #!/usr/bin/env bash
+
+    if ${pkgs.emacs}/bin/emacsclient -e t > /dev/null 2>&1; then
+      # Server is running
+      if [[ -n "$INSIDE_EMACS" ]]; then
+        # Inside Emacs
+        ${pkgs.emacs}/bin/emacsclient "$@"
+      else
+        # Not inside Emacs
+        ${pkgs.emacs}/bin/emacsclient -nw "$@"
+      fi
+    else
+      # Server is not running
+      ${pkgs.emacs}/bin/emacs -q "$@"
+    fi
+  '';
+in
 {
   options.programs.zsh.emacs-editor = {
     enable = mkEnableOption "emacs-editor script for EDITOR";
@@ -11,6 +31,7 @@ with lib;
     };
   };
 
-  config = {
+  config = lib.mkIf cfg.enable {
+    home.packages = [ emacs-editor-script ];
   };
 }
